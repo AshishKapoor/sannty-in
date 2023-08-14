cdd1bc73a127ecc2f929a0c85e5c78b2773f2624
Ashish Kapoor <swiftobjc@gmail.com>
2023-08-14 15:05:44 +0530

    add links to code and dashboard

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -4,6 +4,8 @@ const headerNavLinks = [
   { href: '/projects', title: 'projects' },
   { href: '/about', title: 'about' },
   { href: 'https://www.youtube.com/@ashshkapoor', title: 'youtube' },
+  { href: 'https://code.sannty.in', title: 'code' },
+  { href: 'https://dash.sannty.in', title: 'dashboard' },
 ]
 
 export default headerNavLinks
