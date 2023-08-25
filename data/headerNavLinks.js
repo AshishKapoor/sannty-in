e9c74226802902eafa4e08debd913f6c643b6ace
Ashish Kapoor <swiftobjc@gmail.com>
2023-08-25 10:36:01 +0530

    update monitor url

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -6,7 +6,7 @@ const headerNavLinks = [
   { href: 'https://code.sannty.in', title: 'Code' },
   { href: 'https://dash.sannty.in', title: 'Dash' },
   { href: 'https://www.youtube.com/@ashshkapoor', title: 'Youtube' },
-  { href: 'https://status.sannty.in/status/health', title: 'Status' },
+  { href: 'https://uptime.sannty.in/status/health', title: 'Status' },
 ]
 
 export default headerNavLinks
