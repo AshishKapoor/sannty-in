dfe343a6359cddd83b31dc2dc50d74c462968d1c
Ashish Kapoor <AshishKapoor@users.noreply.github.com>
2023-10-12 11:08:49 +0530

    Update headerNavLinks.js

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -3,8 +3,6 @@ const headerNavLinks = [
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
-  { href: 'https://dash.sannty.in', title: 'Grafana' },
-  { href: 'https://code.sannty.in', title: 'Code Server' },
 ]
 
 export default headerNavLinks
