7b1b5117175d727fba108667ed13c257958131b4
Ashish Kapoor <AshishKapoor@users.noreply.github.com>
2024-07-10 21:57:23 +0530

    Update headerNavLinks.js

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  { href: 'https://uptime.sannty.in/status/health', title: 'Status' },
 ]
 
 export default headerNavLinks
