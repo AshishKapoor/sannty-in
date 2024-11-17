b149a22ed2d08a76dfd63472f49811e811d25fb2
Anton <anton@personal.local>
2024-11-17 03:00:29 +0000

    fix: remove status

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -3,7 +3,6 @@ const headerNavLinks = [
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
-  { href: 'https://uptime.sannty.in/status/health', title: 'Status' },
 ]
 
 export default headerNavLinks
