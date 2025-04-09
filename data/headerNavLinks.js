8ca6874837b1acef4ced41f6a59408378d7c7924
Ashish Kapoor <AshishKapoor@users.noreply.github.com>
2025-04-09 09:51:30 +0530

    Update headerNavLinks.js

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,6 +2,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'All Posts' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
+  { href: 'https://tools.sannty.in/', title: 'Tools' },
   { href: '/about', title: 'About' },
 ]
 
