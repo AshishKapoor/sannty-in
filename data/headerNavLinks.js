550987d5cab8981d26ceab998b1e524cf50c13e5
AshishKapoor <AshishKapoor@users.noreply.github.com>
2025-07-18 17:32:33 +0530

    Update headerNavLinks.js

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -3,7 +3,8 @@ const headerNavLinks = [
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
   { href: 'https://tools.sannty.in/', title: 'Kanban' },
-  { href: 'https://app.sannty.in/', title: 'Budget' },
+  { href: 'https://app.sannty.in/', title: 'Budget Tracker' },
+  { href: 'https://sales-crm.sannty.in/', title: 'My CRM' },
   { href: 'https://emails.sannty.in/', title: 'Emails' },
   { href: '/about', title: 'About' },
 ]
